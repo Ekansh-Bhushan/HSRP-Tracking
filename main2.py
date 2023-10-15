dc565cd7a10a38b5062001f6b20b491a2ae19f09
Kartik-Walia <itzkartikeds3@gmail.com>
2023-10-15 11:20:29 +0530

    New images added for testing

--- main2.py
+++ main2.py
@@ -4,18 +4,19 @@ from matplotlib import pyplot as plt
 import numpy as np 
 import imutils
 import easyocr
+import pytesseract
+import re
 
 
 
 # ================================= Read in image, Grayscale and Blur ========================
-# img = cv2.imread("image2.jpg") # giving issue for now
-img = cv2.imread("image3.jpg")
+img = cv2.imread("image1.jpg")
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.show()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Whenever you're displaying image using matplotlib, it expects RGB (by default img is in BGR)
 plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.show()
 
 
 
@@ -23,7 +24,7 @@ plt.show()
 bfilter = cv2.bilateralFilter(gray, 17, 17, 17) # Noise Reduction
 edged = cv2.Canny(bfilter, 30, 200) # Edge Detection
 plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.show()
 
 
 
@@ -38,7 +39,7 @@ for contour in contours:
     if (len(approx) == 4):
         location = approx
         break
-print(location)
+# print(location)
 
 
 
@@ -47,7 +48,7 @@ mask = np.zeros(gray.shape, np.uint8)
 new_image = cv2.drawContours(mask, [location], 0, 255, -1)
 new_image = cv2.bitwise_and(img, img, mask=mask)
 plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.show()
 
 
 
@@ -77,14 +78,14 @@ cropped_image = gray[x1:x2+1, y1:y2+1]
 # NumPy's array slicing is exclusive of the ending index by default, so adding 1 includes the 
 # last element.
 plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.show()
 
 
 
 # ==================================== Use Easy OCR to read text =============================
 reader = easyocr.Reader(['en'])
 result = reader.readtext(cropped_image)
-print(result)
+# print(result)
 
 
 
@@ -98,5 +99,6 @@ if result:
     res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
     plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
     plt.show()
+    print(plate_text)
 else:
     print("No text found in the result.")
